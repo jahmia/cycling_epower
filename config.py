f3bf47c283bcfc942bf4e4ba7881eaa084657e8a
Mihaja <arfalisoamihaja@gmail.com>
2026-03-04 08:37:43 +0300

    [UPD] Configuration file required

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ import yaml
 
 def read_config():
     """ Read config from file"""
-    with open('config.yaml', encoding="utf-8") as cf_file:
-        config = yaml.safe_load(cf_file.read())
-        return config
+    try:
+        with open('config.yaml', encoding="utf-8") as cf_file:
+            config = yaml.safe_load(cf_file.read())
+            return config
+    except FileNotFoundError:
+        print("Configuration file not found! Aborting.")
